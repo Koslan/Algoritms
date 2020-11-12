b4d278e9b59b9591764fa3254781a8fe145f0284
Kostiantyn Buriak <koslan843@gmail.com>
2020-11-13 01:15:14 +0200

    count sort bencmark

---
+++ src/CountingSort.java
@@ -0,0 +1,123 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountingSort {
+    public static void main(String args[]) throws FileNotFoundException {
+        /*int[] inputArray = {1, 3, 19, 19, 0, 0, 1, 41, 11, 2, 32, 18};
+
+        сountingSort(inputArray);
+
+        int[][] multiArray = {{1, 3, 19}, {99, 0, 1}, {41, 11, 2232, 18}};
+        multiCountingSort(multiArray);*/
+        String fileName1= "/home/koslan/Idea-workspace/test_tasks/test/inputArray1.csv";
+        String fileName2= "/home/koslan/Idea-workspace/test_tasks/test/inputArray2.csv";
+        String fileName3= "/home/koslan/Idea-workspace/test_tasks/test/inputArray3.csv";
+        File file1 = new File(fileName1);
+        File file2 = new File(fileName2);
+        File file3 = new File(fileName3);
+
+        int[] intMas1 = getIntArray(file1);
+        int[] intMas2 = getIntArray(file2);
+        int[] intMas3 = getIntArray(file3);
+
+
+        long startTime1 = System.currentTimeMillis();
+        for(int i = 0; i < 100; i++)
+        сountingSort(intMas1);
+        long timeSpent1 = System.currentTimeMillis() - startTime1;
+
+
+        long startTime2 = System.currentTimeMillis();
+
+        for(int i = 0; i < 100; i++)
+        сountingSort(intMas2);
+        long timeSpent2 = System.currentTimeMillis() - startTime2;
+
+
+        long startTime3 = System.currentTimeMillis();
+
+        for(int i = 0; i < 100; i++)
+        сountingSort(intMas3);
+        long timeSpent3 = System.currentTimeMillis() - startTime3;
+
+
+        System.out.println("программа 1 выполнялась " + timeSpent1 + " миллисекунд" + "\n" + timeSpent1);
+        System.out.println("программа 2 выполнялась " + timeSpent2 + " миллисекунд" + "\n" + timeSpent2);
+        System.out.println("программа 3 выполнялась " + timeSpent3 + " миллисекунд" + "\n" + timeSpent3);
+
+        System.out.println("size 1 " + intMas1.length);
+        System.out.println("size 2 " + intMas2.length);
+        System.out.println("size 3 " + intMas3.length);
+
+        System.out.println("max 1 " + getMax(intMas1));
+        System.out.println("max 2 " + getMax(intMas2));
+        System.out.println("max 3 " + getMax(intMas3));
+
+
+    }
+
+
+    public static int[] getIntArray(File file) throws FileNotFoundException {
+        Scanner inputStream;
+        inputStream = new Scanner(file);
+        String line= inputStream.next();
+        String[] values = line.split(",");
+        int[] mas1 = new int[values.length];
+
+        for(int i = 0; i < values.length; i++){
+            mas1[i] = Integer.parseInt(values[i]);
+        }
+
+        return mas1;
+    }
+
+    public static void сountingSort(int[] mas) {
+            //System.out.println(Arrays.toString(
+                    sort(mas, new int[mas.length], getMax(mas));
+    }
+
+    public static void multiCountingSort(int[][] mas) {
+        for (int i = 0; i < mas.length; i++) {
+            System.out.println(Arrays.toString(sort(mas[i], new int[mas[i].length], getMax(mas[i]))));
+        }
+
+    }
+
+
+    private static int getMin(int[] inputArray) {
+        int result = Integer.MAX_VALUE;
+        for (int i : inputArray) {
+            result = i < result ? i : result;
+        }
+        return result;
+    }
+
+    private static int getMax(int[] inputArray) {
+        int result = Integer.MIN_VALUE;
+        for (int i : inputArray) {
+            result = i > result ? i : result;
+        }
+        return result;
+    }
+
+
+    private static int[] sort(int[] inputArray, int[] resultArray, int k) {
+        k++;
+        int[] C = new int[k];
+
+        for (int i = 0; i < inputArray.length; ++i)
+            ++C[inputArray[i]];
+
+        for (int i = 1; i < k; i++)
+            C[i] += C[i - 1];
+
+        for (int i = inputArray.length - 1; i >= 0; i--) {
+            resultArray[C[inputArray[i]] - 1] = inputArray[i];
+            --C[inputArray[i]];
+        }
+
+        return resultArray;
+    }
+}
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,95 +3,28 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] inputArray = {1, 3, 19, -99, 0, 1, -41, 11, 2232, 18};
-        //mergeSort(inputArray, 0, inputArray.length - 1);
-        //System.out.println(Arrays.toString(inputArray));
+
+        mergeArray(inputArray);
 
 
-        sort(inputArray, 0, inputArray.length - 1);
-
-        System.out.println("\nSorted array");
-        printArray(inputArray);
+        int[][] multiArray = {{1, 3, 19},{ -99, 0, 1},{ -41, 11, 2232, 18}};
+        mergeMultiArray(multiArray);
     }
 
-    public static void merge(int arr[], int l, int m, int r)
-    {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        /* Create temp arrays */
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarry array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of R[] if any */
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+    public static void mergeArray(int[] mas) {
+            mergeSort(mas, 0, mas.length - 1);
+            System.out.println(Arrays.toString(mas));
     }
 
-    // Main function that sorts arr[l..r] using
-    // merge()
-    public static  void sort(int arr[], int l, int r)
-    {
-        if (l < r) {
-            // Find the middle point
-            int m = (l + r) / 2;
-
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
+    public static void mergeMultiArray(int[][] mas) {
+        for(int i = 0; i < mas.length; i++) {
+            mergeSort(mas[i], 0, mas[i].length - 1);
+            System.out.println(Arrays.toString(mas[i]));
         }
+
     }
 
-    /* A utility function to print array of size n */
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-}
-
-
-    /*public static void mergeSort(int A[], int p, int r) {
+    public static void mergeSort(int A[], int p, int r) {
         if (p < r) {
             int m = (p + r) / 2;
 
@@ -101,33 +34,46 @@ public class MergeSort {
         }
     }
 
-    public static void merge(int[] A, int p, int q, int r) {
-        int n1 = q - p + 1;
-        int n2 = r - q;
+    public static void merge(int A[], int p, int q, int r)
+    {
+        int n1 = q - p + 1; // size subArray1
+        int n2 = r - q;  // size subArray2
 
-        int[] L = new int[n1];
-        int[] R = new int[n2];
+        int L[] = new int[n1];
+        int R[] = new int[n2];
 
-        for(int i = 1; i < n1; ++i) {
+
+        for (int i = 0; i < n1; ++i) { //Copy data to temp array1
             L[i] = A[p + i];
         }
-
-        for(int i = 1; i < n2; ++i) {
-            L[i] = A[q + i + 1];
-        }
-
+        for (int i = 0; i < n2; ++i) { //Copy data to temp array2
+            R[i] = A[q + i + 1];
+    }
         int left = 0;
         int right = 0;
+        int k = p;
 
-        for(int i = 1; left < n1 && right < n2; i++) {
+        for(; left < n1 && right < n2; k++) {
             if(L[left] <= R[right]){
-                A[i] = L[left];
+                A[k] = L[left];
                 left++;
             } else {
-                A[i] = R[right];
+                A[k] = R[right];
                 right++;
             }
         }
+        //Copy remaining elements of L[] if any
+        while (left < n1) {
+            A[k] = L[left];
+            left++;
+            k++;
+        }
 
+        // Copy remaining elements of R[] if any
+        while (right < n2) {
+            A[k] = R[right];
+            right++;
+            k++;
+        }
     }
-}*/
+}
