ea4fb53c5d31c3292df1b1a76af7ee2701ce24b3
Kostiantyn Buriak <koslan843@gmail.com>
2020-11-19 00:08:35 +0200

    Algoritms
    - 3 1/2 second task homework

--- src/Algorithms/Recursion3/BinaryPrint.java
+++ src/Algorithms/Recursion3/BinaryPrint.java
@@ -1,7 +1,46 @@
 package Algorithms.Recursion3;
 
 public class BinaryPrint {
-    public static void main(String[] args) {
 
+    /*
+    Реализуйте с помощью рекурсии и итеративным методом
+    вывод десятичного числа n введенного пользователем
+    на экран в двоичной системе счисления.
+    */
+
+    public static void main(String[] args) {
+        System.out.println(printIterative(1023));
+        System.out.println(printIterative(1024));
+        System.out.println(printIterative(24));
+        System.out.println(printIterative(1));
+        System.out.println(printIterative(0));
+        System.out.println(printIterative(291_321));
+    }
+
+    public static String printIterative(int n) {
+        if (n == 0 || n == 1)
+            return  "" + n;
+
+        StringBuffer result = new StringBuffer();
+        int power = 1;
+
+        while(power < n)
+            power *= 2;
+
+
+        int n2 = n;
+        int cur = 0;
+        result.append(n2 > 0 ? 0 : 1);
+
+        while(power > 0) {
+            if(n2 >= power) {
+            result.append(1);
+                n2 -= power;
+            } else {
+                result.append(0);
+            }
+            power /= 2;
+        }
+        return result.toString();
     }
 }
