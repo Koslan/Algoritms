23123ecb3816203d4e82a315fb0e024ceff3dfdf
Kostiantyn Buriak <koslan843@gmail.com>
2020-11-26 23:54:48 +0200

    Algoritms
    - SimplyLinkedList finish

--- src/Algorithms/DataStructures4/SinglyLinkedList.java
+++ src/Algorithms/DataStructures4/SinglyLinkedList.java
@@ -1,5 +1,8 @@
 package Algorithms.DataStructures4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 
 public class SinglyLinkedList {
@@ -36,10 +39,10 @@ public class SinglyLinkedList {
 
         if(size == 0) {
             firstElem = elem;
-        } else {
-            elem.setNextElem(this.firstElem);
+            lastElem = elem;
         }
 
+        lastElem.setNextElem(elem);
         lastElem = elem;
         elem.setValue(value);
         size++;
@@ -55,15 +58,22 @@ public class SinglyLinkedList {
         size++;
     }
 
-    public int getValueByIndex(int index) {
+    public int getValueByIndex(int index) throws Exception {
         int value = 0;
+
+        if(index > size)
+            throw new Exception("error: index > list.size");
+
+        SinglyLinkedListNode result = firstElem;
+        for(int i = 0; i < index; i++)
+            result = result.getNextElem();
+
+        value = result.getValue();
 
         return value;
     }
 
     public SinglyLinkedListNode getElemByIndex(int index) throws Exception {
-        int value = 0;
-
         if(index > size)
             throw new Exception("error: index > list.size");
 
@@ -74,8 +84,98 @@ public class SinglyLinkedList {
         return result;
     }
 
+    public SinglyLinkedListNode removeElemByIndex(int index) throws Exception {
+        if(size == 0)
+            return null;
 
-    public static void main(String[] args) {
+        if(index == 0) {
+            SinglyLinkedListNode elem = firstElem;
+            firstElem = firstElem.getNextElem();
 
+            if(size == 1) {
+                lastElem = null;
+                firstElem = null;
+            }
+            size--;
+            return elem;
+
+        }
+
+        if(index == size)
+            return lastElem;
+
+        if(index > size)
+            throw new Exception("error: index > list.size");
+
+        SinglyLinkedListNode curr = firstElem;
+        SinglyLinkedListNode prev = firstElem;
+        for(int i = 1; i <= index; i++) {
+            prev = firstElem;
+            curr = firstElem.getNextElem();
+        }
+
+        prev.setNextElem(curr.getNextElem());
+        size--;
+
+
+        return curr;
+    }
+
+
+
+    public static void printValue(String value) {
+        System.out.printf("%4s|", value);
+    }
+
+    public void print() throws Exception {
+        for (int i = 0; i < size; i++)
+            printValue("" + getValueByIndex(i));
+
+        System.out.println();
+        for (int i = 0; i < size; i++)
+            printValue("----");
+
+        System.out.println();
+
+        for (int i = 0; i < size; i++) {
+                printValue("" + i);
+        }
+    }
+
+
+    public static String console(String str, SinglyLinkedList list) throws Exception {
+        if (str.length() > 5 && str.substring(0, 4).equals("addf")) {
+            String pS = str.substring(5, str.length());
+            int pars = Integer.parseInt(pS);
+            list.addFirst(pars);
+        } else if (str.length() > 5 && str.substring(0, 4).equals("addl")) {
+            String pS = str.substring(5, str.length());
+            int pars = Integer.parseInt(pS);
+            list.addLast(pars);
+        } else if (str.length() > 5 && str.substring(0, 3).equals("get")) {
+            String pS = str.substring(4,  str.length());
+            int pars = Integer.parseInt(pS);
+            list.getValueByIndex(pars);
+        } else if (str.length() > 5 && str.substring(0, 6).equals("remove")) {
+            String pS = str.substring(7,  str.length());
+            int pars = Integer.parseInt(pS);
+            list.removeElemByIndex(pars);
+        } else if (str.length() > 4 && str.substring(0, 5).equals("print")) {
+            list.print();
+        }
+
+
+        return "";
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        while (true) {
+            System.out.println(console(br.readLine(), list));
+        }
     }
 }
